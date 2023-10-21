b4b09149c29f2bb38686591b1ccb38a2fae2b94b
daniel <farzidaniel16@gmail.com>
2023-10-21 23:21:00 +0300

    bug fixed

--- src/config/api.js
+++ src/config/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export default axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://srv.netbest.tk/api",
 });
