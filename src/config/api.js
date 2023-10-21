967a6076b92e3888258acd669913a3bc16f7e433
daniel <farzidaniel16@gmail.com>
2023-10-21 17:54:20 +0300

    ready to deploy

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-
+axios.defaults.withCredentials = true;
 export default axios.create({
   baseURL: "https://snap-api.onrender.com/api/",
+  "Access-Control-Allow-Origin": "*",
 });
