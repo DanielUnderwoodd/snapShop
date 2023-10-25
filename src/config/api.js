1aa521c7174986282b510fd6f089c692613725f6
daniel <farzidaniel16@gmail.com>
2023-10-25 21:09:07 +0300

    api changed

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export default axios.create({
-  baseURL: "https://snapshop-api-production.up.railway.app/api",
+  baseURL: "https://srv.netbest.tk/api",
 });
 
 //https://srv.netbest.tk/api
