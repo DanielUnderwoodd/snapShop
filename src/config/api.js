31fb2c2e79cfc8468fddc6da483364e554348707
daniel <farzidaniel16@gmail.com>
2023-11-02 20:56:25 +0200

    fixed: responsive bugs

--- src/config/api.js
+++ src/config/api.js
@@ -7,37 +7,37 @@ const api = axios.create({
 
 //https://srv.netbest.tk/api
 
-api.interceptors.request.use(
-  (config) => {
-    config.metadata = { startTime: new Date() }; // Add a startTime property to the request config
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// api.interceptors.request.use(
+//   (config) => {
+//     config.metadata = { startTime: new Date() }; // Add a startTime property to the request config
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 // Add a response interceptor
-api.interceptors.response.use(
-  (response) => {
-    // Calculate the delay
+// api.interceptors.response.use(
+//   (response) => {
+//     // Calculate the delay
 
-    const endTime = new Date();
-    const startTime = response.config.metadata.startTime;
-    const delay = endTime - startTime;
+//     const endTime = new Date();
+//     const startTime = response.config.metadata.startTime;
+//     const delay = endTime - startTime;
 
-    // Display the delay to the user (you can customize this part)
+//     // Display the delay to the user (you can customize this part)
 
-    // Check if the delay exceeds 2 seconds and throw an error
-    if (delay > 2000) {
-      throw new Error(`Request took too long, please try again `);
-    }
+//     // Check if the delay exceeds 2 seconds and throw an error
+//     if (delay > 10000) {
+//       throw new Error(`Request took too long, please try again `);
+//     }
 
-    return response;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+//     return response;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 export default api;
