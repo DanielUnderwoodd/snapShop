7c531379244ecb68c44cd60afacc71fe95c8c337
Daniel farzi <danielfarzi@Daniels-MBP.fritz.box>
2023-12-02 18:18:50 +0200

    fixed: adding skeleton to front page

--- src/components/mainSection/MainSection.js
+++ src/components/mainSection/MainSection.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { Container } from "react-bootstrap";
 
 import { connect } from "react-redux";
 import { get_product } from "../../actions/public/publicAction";
 import CarouselCustom from "../../config/CarouselCustom";
+import SkeletonLoad from "../SkeletonLoad";
 import ECommerceCard from "./ECommerceCard";
 import shortid from "shortid";
 import "../../componentsCss/Slider.css";
@@ -27,38 +28,43 @@ class MainSection extends Component {
   render() {
     return (
       <div className="slider">
-        <Container>
-          {this.props.products.map((category, index) => {
-            return (
-              <React.Fragment key={shortid.generate()}>
-                <div style={{ display: "flex" }}>
-                  <h3>{category.categoryName}</h3>
-                </div>
-                <div
-                  className={
-                    this.props.products.length === index + 1
-                      ? "last-carousel"
-                      : ""
-                  }>
-                  <CarouselCustom>
-                    {category.products.map((product) => {
-                      let { id, image, title, price } = product;
-                      return (
-                        <ECommerceCard
-                          id={id}
-                          img={image}
-                          text={title}
-                          price={price}
-                          key={shortid.generate()}
-                        />
-                      );
-                    })}
-                  </CarouselCustom>
-                </div>
-              </React.Fragment>
-            );
-          })}
-        </Container>
+        {this.props.products ? (
+          <Container>
+            {this.props.products.map((category, index) => {
+              return (
+                <React.Fragment key={shortid.generate()}>
+                  <div style={{ display: "flex" }}>
+                    <h3>{category.categoryName}</h3>
+                  </div>
+                  <div
+                    className={
+                      this.props.products.length === index + 1
+                        ? "last-carousel"
+                        : ""
+                    }
+                  >
+                    <CarouselCustom>
+                      {category.products.map((product) => {
+                        let { id, image, title, price } = product;
+                        return (
+                          <ECommerceCard
+                            id={id}
+                            img={image}
+                            text={title}
+                            price={price}
+                            key={shortid.generate()}
+                          />
+                        );
+                      })}
+                    </CarouselCustom>
+                  </div>
+                </React.Fragment>
+              );
+            })}
+          </Container>
+        ) : (
+          <SkeletonLoad />
+        )}
       </div>
     );
   }
--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://srv.netbest.tk/api",
+  baseURL: "http://localhost:4000/api",
   withCredentials: true,
 });
 
