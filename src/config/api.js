3bb9db37347476fbc0706653a9402daa8f4150cb
Daniel <42712911+DanielUnderwoodd@users.noreply.github.com>
2024-05-16 22:09:13 +0300

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://e-commerce.adaptable.app/api",
+  baseURL: "https://srv.danielportfolio.shop/api",
   withCredentials: true,
 });
 
